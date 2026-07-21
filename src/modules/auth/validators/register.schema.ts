0c95c09763a970fbd022eaa77e08d761b536ff91
JE <jguevarra0307@yahoo.com>
2026-07-21 22:19:00 +0800

    Added max length in RegisterSchema

--- src/modules/auth/validators/register.schema.ts
+++ src/modules/auth/validators/register.schema.ts
@@ -2,6 +2,6 @@ import { z } from "zod"
 
 export const RegisterSchema = z.object({
   email: z.email(),
-  password: z.string().min(8)
+  password: z.string().min(8).max(72)
 })
 export type RegisterDto = z.infer<typeof RegisterSchema>
